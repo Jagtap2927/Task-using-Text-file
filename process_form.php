5525284f56a1b6a785d5631f37b168d767e6ffdc
Omkar Jagtap <107162121+Jagtap2927@users.noreply.github.com>
2024-05-03 21:07:45 +0530

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,18 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the data from the form data
+    // Retrieve the data from theform data
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    $data = array($name, $email, $message);
-    $file = 'contact_data.csv';
-    $fp = fopen($file, 'a');
-    fputcsv($fp, $data);
-    fclose($fp);
-
-    echo "Data saved successfully!";
+    $formattedData = "Name: $name\nEmail: $email\nMessage: $message\n";
+    $file = "contact_data.txt";
+    if (file_put_contents($file, $formattedData, FILE_APPEND | LOCK_EX) !== false) {
+        echo "Data saved successfully.";
+    } else {
+        echo "Error occurred. Data not saved.";
+    }
 } else {
-    
     header("Location: contact_form.html");
     exit();
 }
